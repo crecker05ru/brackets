c9369ae06ebfe9beb503574f952a11beb09d9aed
Anvar <fort2652@gmail.com>
2023-01-26 22:11:17 +0300

    feat: ver 2

--- src/check.js
+++ src/check.js
@@ -9,11 +9,28 @@ const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 const check = require('./index')
 
 console.log('check1 true',check('|()|',config5))
-console.log('check2 false',check('[]][[]',config2))
+console.log('check17 false',check('[]][[]',config2))
+console.log('check13 false',check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6))
 // it('should check if brackets sequence is correct 9', () => {
 //   assert.equal(check('|()|', config5), true);
 // });
 
 // it('should check if brackets sequence is not correct 17', () => {
 //   assert.equal(check('[]][[]', config2), false);
+// });
+
+// it('should check if brackets sequence is not correct 16', () => {
+//   assert.equal(check('[]][[]', config3), false);
+// });
+
+// it('should check if brackets sequence is correct 13', () => {
+//   assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), false);
+// });
+
+// it('should check if brackets sequence is not correct 17', () => {
+//   assert.equal(check('[]][[]', config2), false);
+// });
+
+// it('should check if brackets sequence is not correct 18', () => {
+//   assert.equal(check('([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()', config7), false);
 // });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // console.log('str',str)
+  console.log('str',str)
 
-  // let stack = []
-  // let bracketsMap = {}
-  // bracketsConfig.forEach(bracket => {
-  //   if(!bracketsMap[bracket[0]]){
-  //     bracketsMap[bracket[0]] = bracket[1]
-  //   }
-  // })
-  // let brackets = str.split('')
-  // console.log('stack',stack)
-  // console.log('bracketsMap',bracketsMap)
-  // console.log('bracketsConfig',bracketsConfig)
-  // let last = stack[stack.length - 1]
+  let stack = []
+  let bracketsMap = {}
+  bracketsConfig.forEach(bracket => {
+    // if(bracket[0] === bracket[1]){
+    //   bracketsMap[bracket[1] + ' close'] = bracket[0] + ' open'
+    // } else {
+    //   bracketsMap[bracket[1]] = bracket[0]
+    // }
+
+    if(!bracketsMap[bracket[1]]){
+      bracketsMap[bracket[1]] = bracket[0]
+    }
+
+  })
+  // let [openBracket, closeBracket] = Object.entries(bracketsMap)
+  // console.log('openBracket, closeBracket',openBracket, closeBracket)
+  let closeBrackets = Object.keys(bracketsMap)
+  let openBrackets = Object.values(bracketsMap)
+  console.log('openBrackets,closeBrackets',openBrackets,closeBrackets)
+  let brackets = str.split('')
+  console.log('stack',stack)
+  console.log('bracketsMap',bracketsMap)
+  console.log('bracketsConfig',bracketsConfig)
+  
+
+  brackets.forEach(bracket => {
+    console.log('bracket',bracket)
+    console.log('iteration stack',stack)
+    if(openBrackets.includes(bracket) && (!stack.includes(bracket))){
+      console.log('openBrackets.includes(bracket) && (!stack.includes(bracket))',bracket, openBrackets.includes(bracket) && (!stack.includes(bracket)))
+      stack.push(bracket)
+    }else {
+      console.log('else',bracket)
+      if(stack.length === 0){
+        console.log('stack.length === 0',stack.length === 0)
+        return false
+      }
+
+      let last = stack[stack.length - 1]
+
+      if(bracketsMap[bracket] === last ){
+        stack.pop()
+      }else {
+        return false
+      }
+    }
+  })
+
   // brackets.forEach(bracket => {
   //   // bracketsConfig.forEach(config => {
   //     console.log('bracket',bracket)
@@ -34,33 +70,34 @@ module.exports = function check(str, bracketsConfig) {
   //     }
   //   })
 
-  // // });
-  // console.log('stack after',stack)
-  // return stack.length === 0
+  // });
+  console.log('stack after',stack)
+  return stack.length === 0
 
 
 
-  let bracketMaps = {}
-  for(let i = 0;i < str.length; i++){
-    bracketsConfig.forEach(bracket => {
-      // if(bracketMaps[str[i]]){
-        // bracketMaps[str[i]] = 'closed'
-        if(str[i] === bracket[0]){
-          bracketMaps[bracket[0]] = 'opened'
-        // }
-      } else {
-        // bracketMaps[str[i]] = 'opened'
-        // if(str[i] === bracket[0]){
-          bracketMaps[bracket[0]] = 'closed'
-        // }
-      }
-    })
+//   let bracketMaps = {}
+//   for(let i = 0;i < str.length; i++){
+//     bracketsConfig.forEach(bracket => {
+//       // if(bracketMaps[str[i]]){
+//         // bracketMaps[str[i]] = 'closed'
+//         if(str[i] === bracket[0]){
+//           bracketMaps[bracket[0]] = 'opened'
+//         // }
+//       } else {
+//         // bracketMaps[str[i]] = 'opened'
+//         // if(str[i] === bracket[0]){
+//           bracketMaps[bracket[0]] = 'closed'
+//         // }
+//       }
+//     })
 
-  }
-  console.log('bracketMaps',bracketMaps)
- let answer =   bracketsConfig.every( bracket => bracketsConfig[bracket[0]] === bracketsConfig[bracket[1]])
- console.log('answer',answer)
- return answer
+//   }
+//   console.log('bracketMaps',bracketMaps)
+//  let answer =   bracketsConfig.every( bracket => bracketsConfig[bracket[0]] === bracketsConfig[bracket[1]])
+//  console.log('answer',answer)
+//  return answer
+
 }
 
 
